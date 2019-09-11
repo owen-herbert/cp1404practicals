bb690fd7901d3a5c85296101827ee8673a1f5492
Owen <owen.herbert@my.jcu.edu.au>
2019-09-11 14:47:53 +1000

    Added comments

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,6 +1,7 @@
 """CP1404/CP5632 Practical - Programming Language."""
 
 class ProgrammingLanguage:
+    """Programming language information."""
     def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
@@ -8,12 +9,15 @@ class ProgrammingLanguage:
         self.year = year
 
     def __str__(self):
+        """Return string programming information."""
         return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
 
     def is_dynamic(self):
+        """Determine if language is dynamically typed/"""
         return self.typing == "Dynamic"
 
         def run_test():
+            """Run test on ProgrammingLanguage class."""
             ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
             python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
             visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
