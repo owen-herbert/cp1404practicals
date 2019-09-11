1b46de9c91bbb2e5e77d59bb273e390bf156dbda
Owen <owen.herbert@my.jcu.edu.au>
2019-09-11 14:55:55 +1000

    Completed modifications

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,1 +1,22 @@
 """CP1404/CP5632 Practical - Guitar."""
+
+YEAR = 2019
+VINTAGE_AGE = 50
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(selfs):
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return YEAR - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        return self.year < other.year
