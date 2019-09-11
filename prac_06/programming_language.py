cd621f5d102f09dc543e0512a35c9a460aea3e98
Owen <owen.herbert@my.jcu.edu.au>
2019-09-11 14:49:33 +1000

    Code cleanup

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,6 +2,7 @@
 
 class ProgrammingLanguage:
     """Programming language information."""
+
     def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
@@ -10,14 +11,17 @@ class ProgrammingLanguage:
 
     def __str__(self):
         """Return string programming information."""
+
         return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
 
     def is_dynamic(self):
         """Determine if language is dynamically typed/"""
+
         return self.typing == "Dynamic"
 
         def run_test():
             """Run test on ProgrammingLanguage class."""
+
             ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
             python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
             visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
