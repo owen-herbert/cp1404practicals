31da0c6dbf5f028426a98efc48ace9a379800211
Owen <owen.herbert@my.jcu.edu.au>
2019-09-18 14:28:06 +1000

    Code cleanup

--- prac_07/taxi.py
+++ prac_07/taxi.py
@@ -2,18 +2,18 @@
 CP1404/CP5632 Practical
 Car class
 """
-from prac_06.car import Car
 
+from prac_06.car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
     price_per_km = float(1.23)
 
-    def __init__(self, name, fuel, price_per_km):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
+        #self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
--- prac_07/taxi_test.py
+++ prac_07/taxi_test.py
@@ -1,8 +1,10 @@
+"""Taxi Test"""
+
 from prac_07.taxi import Taxi
 
 def main():
     """Test Class for taxi"""
-    taxi = Taxi("Prius 1", 100, 1.23)
+    taxi = Taxi("Prius 1", 100)
     taxi.drive(40)
     print(taxi)
     taxi.start_fare()
