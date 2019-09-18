f9ad85a35ee6fe66961e6ec82f793d614f5bf7f5
Owen <owen.herbert@my.jcu.edu.au>
2019-09-18 14:21:17 +1000

    Completed modifications

---
+++ prac_07/taxi_test.py
@@ -0,0 +1,10 @@
+from prac_07.taxi import Taxi
+
+def main():
+    """Test Class for taxi"""
+    taxi = Taxi("Prius 1", 100, 1.23)
+    taxi.drive(40)
+    print(taxi)
+    taxi.start_fare()
+    taxi.drive(100)
+    print(taxi)
