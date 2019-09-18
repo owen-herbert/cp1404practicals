c355be3e969832eafc4a4ea0069b9a949ccfc450
Owen <owen.herbert@my.jcu.edu.au>
2019-09-18 14:37:05 +1000

    Completed modifications

---
+++ prac_07/unreliable_car.py
@@ -0,0 +1,20 @@
+"""Unreliable Car Class"""
+
+from prac_06.car import Car
+from random import randint
+
+class UnreliableCar(Car):
+    reliability = float(1.00)
+
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        randomNum = randint(1, 100)
+
+        if randomNum >= self.reliability:
+            distance = 0
+
+        distanceDriven = super().drive(distance)
+        return distanceDriven
