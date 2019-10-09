c1db3fa72420f2ae538bb7c0feedf70b0dd02d20
jc605320 <owen.herbert@my.jcu.edu.au>
2019-10-09 14:09:18 +1000

    Instructions Complete

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,14 @@
+"""taxi_test.py"""
+
+from prac_08.taxi import Taxi
+
+def main():
+    """Test Taxi Class"""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+main()
