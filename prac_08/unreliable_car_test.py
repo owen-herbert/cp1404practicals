3afd30e5dd400bdeb5e5165bb06a759e75a26450
jc605320 <owen.herbert@my.jcu.edu.au>
2019-10-09 14:25:32 +1000

    Instructions Complete

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -1,1 +1,18 @@
-"""Unreliable Car Test"""
+"""unreliable_car_test.py"""
+
+from prac_08.unreliable_car import UnreliableCar
+
+def main():
+    """Test Unreliable Car"""
+    reliable_car = UnreliableCar("Toyota", 100, 88)
+    unreliable_car = UnreliableCar("Volkswagen", 100, 13)
+
+    for i in range(1, 10):
+        print("Driving two vehicles {0}km:".format(i))
+        print("{0} drove {1}km".format(reliable_car.name, reliable_car.drive(i)))
+        print("{0} drove {1}km".format(unreliable_car.name, unreliable_car.drive(i)))
+
+    print(reliable_car)
+    print(unreliable_car)
+
+main()
