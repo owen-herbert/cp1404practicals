1f6c6ce37aaaaef1165d259136811cb7f1a58515
jc605320 <owen.herbert@my.jcu.edu.au>
2019-10-09 14:12:02 +1000

    Add price_per_km variable to taxi class

--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -8,6 +8,8 @@ from prac_08.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
+    price_per_km = 1.23
+
     def __init__(self, name, fuel, price_per_km):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
