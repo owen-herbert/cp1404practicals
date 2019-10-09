ce85da660d7fb9d68e336d4a25563e5aec13cf1f
jc605320 <owen.herbert@my.jcu.edu.au>
2019-10-09 14:33:30 +1000

    Instructions Complete

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -1,2 +1,12 @@
 """silver_service_taxi_test.py"""
 
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    """Test SilverServiceTaxi"""
+    taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
+    taxi.drive(19)
+    print(taxi)
+    print(taxi.get_fare())
+
+main()
