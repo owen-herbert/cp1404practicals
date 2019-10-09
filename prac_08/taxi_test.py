4fbb2cda66a34526f57204fa274690ebaae5162d
jc605320 <owen.herbert@my.jcu.edu.au>
2019-10-09 14:14:20 +1000

    Update client code to match class requirements

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -4,7 +4,7 @@ from prac_08.taxi import Taxi
 
 def main():
     """Test Taxi Class"""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
